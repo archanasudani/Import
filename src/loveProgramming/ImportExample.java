3663b061e524054a98c140303a7599f8ca39a136
archana sudani <archanasudani@gmail.com>
2019-01-07 20:41:42 +0000

    My First Commit

---
+++ src/loveProgramming/ImportExample.java
@@ -0,0 +1,22 @@
+package loveProgramming;
+
+import c.MyClass;
+
+import java.math.RoundingMode;
+import java.util.Random;
+
+
+public class ImportExample {
+           public static void main(String[] args) {
+            Random r = new Random();
+//        Date d ;
+            java.util.Date da;
+            java.sql.DriverManager driverManager;
+    System.out.println(r.nextInt(100));
+//            System.out.println(RoundingMode.valueOf(6));
+
+       MyClass abc = new MyClass();
+        }
+
+    }
+
